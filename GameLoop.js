3a7dc6e9a7cd37c7b865423726ad0673cc7331bc
Olachi-eze <bearcatpump@gmail.com>
2025-08-31 18:32:22 -0400

    Update GameLoop.js

--- GameLoop.js
+++ GameLoop.js
@@ -10,7 +10,16 @@ function updateEntities() {
 }
 
 function renderScene() {
-  // Draw blocks, players, UI
+  document.querySelector("button").addEventListener("click", () => {
+  const inputs = document.querySelectorAll("input");
+  const name = inputs[0].value;
+  const x = parseInt(inputs[1].value);
+  const y = parseInt(inputs[2].value);
+  const z = parseInt(inputs[3].value);
+  console.log(`Teleporting ${name} to (${x}, ${y}, ${z})`);
+  // You can link this to your TypeScript backend or game engine
+});
+  
 }
 
 gameLoop();
